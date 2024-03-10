9ae12861cb331bcf63bf1216a7834b7dd032fa74
Sahil-18 <sahilpurohit18@gmail.com>
2024-03-10 19:48:43 -0400

    Issues

--- bitTorrent/seeder.py
+++ bitTorrent/seeder.py
@@ -1,10 +1,3 @@
-# # Write a bittorrent seeder
-# # The leecher is on different machine
-# # The seeder is on different machine
-# # Seeder needs to send multiple files to the leecher when requested
-# # The leecher will request the file from the seeder
-
-
 import libtorrent as lt
 import sys
 import time
@@ -33,8 +26,11 @@ def seed_torrent(torrent_file, save_path, port):
     total_data_transfered = 0
     total_payload_transfered = 0
     
-    # h = ses.add_torrent({'ti': lt.torrent_info(torrent_file), 'save_path': save_path}) 
+    h = ses.add_torrent({'ti': lt.torrent_info(torrent_file), 'save_path': save_path}) 
     while True:
+        # ses = lt.session()
+        # ses.listen_on(port, port + 10)
+ 
         h = ses.add_torrent({'ti': lt.torrent_info(torrent_file), 'save_path': save_path})
         print(name , " Seeding... ", i)
         s = h.status()
@@ -43,14 +39,14 @@ def seed_torrent(torrent_file, save_path, port):
             s = h.status()
             total_data_transfered = s.total_upload
             total_payload_transfered = s.total_payload_upload
-            # state_str = ['queued', 'checking', 'downloading metadata', 'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
+            state_str = ['queued', 'checking', 'downloading metadata', 'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
 
-            # print('\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
-            #     (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state]))
+            print('\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
+                (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state]))
             sys.stdout.flush()
-
             time.sleep(1)
         ses.remove_torrent(h)
+        ses.post_torrent_updates()
         print(f"Seeding {i} of {name} complete")
         i+=1
         print("Upload data : ", total_data_transfered, name)
@@ -62,7 +58,8 @@ file_paths = ['A_10kB', 'A_100kB', 'A_1MB', 'A_10MB']
 # file_paths = ['A_10kB']
 
 # Tracker URL
-tracker_url = "udp://tracker.openbittorrent.com:80/announce"
+# tracker_url = "tcp://tracker.openbittorrent.com:80/announce"
+tracker_url = "udp://tracker.opentrackr.org:1337/announce"
 
 # Ports for each torrent
 ports = [6881, 6882, 6883, 6884]
@@ -74,7 +71,7 @@ for i, file_path in enumerate(file_paths):
         f.write(lt.bencode(torrent_data))
 
 # while True:
-#     seed_torrent("A_10kB.torrent", ".", 6881)
+# seed_torrent("A_10kB.torrent", ".", 6881)
 
 # Seed each torrent in a separate thread
 
