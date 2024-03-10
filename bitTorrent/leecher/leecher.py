cfda6259a1604538caae96b4f26a07687ae1fd6d
Sahil-18 <sahilpurohit18@gmail.com>
2024-03-09 23:22:48 -0500

    Issues

--- bitTorrent/leecher/leecher.py
+++ bitTorrent/leecher/leecher.py
@@ -7,16 +7,21 @@ import os
 
 def download_torrent(file_path, torrent_file,num_requests, file_size):
     ses = lt.session()
-    ses.listen_on(6881, 6891)
+    ses.listen_on(7072, 7082)
     RTT= []
     sizes = []
     thptvalues = []
+    total_data_transfered = 0
+    total_payload = 0
 
-    for _ in range(num_requests):
+    for i in range(num_requests):
+        print("Requesting ", i + 1)
         if os.path.exists(file_path):
             os.remove(file_path)
         # Add a torrent
         start = timeit.default_timer()
+        total_data_transfered = 0
+        total_payload = 0
         e = lt.bdecode(open(torrent_file , 'rb').read())
         info = lt.torrent_info(e)
         h = ses.add_torrent({'ti': info, 'save_path': '.'})
@@ -25,27 +30,32 @@ def download_torrent(file_path, torrent_file,num_requests, file_size):
         while not s.is_seeding:
             s = h.status()
             total_data_transfered = s.total_download
+            total_payload = s.total_payload_download
+            #state_str = ['queued', 'checking', 'downloading metadata',
+                         #'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
 
-            state_str = ['queued', 'checking', 'downloading metadata',
-                         'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
-
-            print('\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
-                  (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state]))
+            #print('\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
+                  #(s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state]))
             sys.stdout.flush()
 
         end = timeit.default_timer()
+        #total_payload = s.total_payload_download
+        total_data_transfered = s.total_download
+        total_payload = s.total_payload_download
+        print("Total Data Transfered : ", total_data_transfered)
+        print("Total Payload : ", total_payload)
+        print("Total Done : ", s.total_done)
         RTT.append(end-start)
         sizes.append(total_data_transfered/file_size)
         thptvalues.append(file_size*0.008/(end-start))
-
-    # Leave the session
+        print("Completed ", i + 1, " request")
     ses.remove_torrent(h)
 
         # Create a csv file to store RTT, throughput and total data transfered with name as filename_results.csv
     with open(file_path + "_results.csv", 'w') as file:
         file.write("RTT,Throughput,TotalDataTransfered\n")
         for i in range(num_requests):
-            file.write(str(sizes[i]) + "," + str(thptvalues[i]) + "," + str(sizes[i]) + "\n")
+            file.write(str(RTT[i]) + "," + str(thptvalues[i]) + "," + str(sizes[i]) + "\n")
 
     # Calculate average RTT, throughput and total data transfered also standard Deviation
     # Also ensure that standard deviation is not calculated for 1 iteration
@@ -63,9 +73,9 @@ def download_torrent(file_path, torrent_file,num_requests, file_size):
 torrent_files = ['./A_10kB.torrent', './A_100kB.torrent', './A_1MB.torrent', './A_10MB.torrent']
 file_paths = ['A_10kB', 'A_100kB', 'A_1MB', 'A_10MB']
 
-result_A_10kB = download_torrent(file_paths[0], torrent_files[0], 333, 10240)
-result_A_100kB = download_torrent(file_paths[1], torrent_files[1], 33, 102400)
-result_A_1MB = download_torrent(file_paths[2], torrent_files[2], 3,1048576)
+result_A_10kB = download_torrent(file_paths[0], torrent_files[0], 10, 10240)
+result_A_100kB = download_torrent(file_paths[1], torrent_files[1], 10, 102400)
+result_A_1MB = download_torrent(file_paths[2], torrent_files[2], 10,1048576)
 result_A_10MB = download_torrent(file_paths[3], torrent_files[3], 1, 10485760)
 
 print("\nResults for A_10KB file")
--- bitTorrent/seeder.py
+++ bitTorrent/seeder.py
@@ -26,23 +26,40 @@ def create_torrent(file_path, tracker_url):
 
 # Function to seed a torrent
 def seed_torrent(torrent_file, save_path, port):
+    name = torrent_file.split(".")[0]
     ses = lt.session()
     ses.listen_on(port, port + 10)
+    i = 0
+    total_data_transfered = 0
+    total_payload_transfered = 0
     
-    h = ses.add_torrent({'ti': lt.torrent_info(torrent_file), 'save_path': save_path}) 
-
+    # h = ses.add_torrent({'ti': lt.torrent_info(torrent_file), 'save_path': save_path}) 
     while True:
+        h = ses.add_torrent({'ti': lt.torrent_info(torrent_file), 'save_path': save_path})
+        print(name , " Seeding... ", i)
         s = h.status()
-        state_str = ['queued', 'checking', 'downloading metadata', 'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
 
-        print('\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
-            (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state]))
-        sys.stdout.flush()
+        while not s.is_seeding:
+            s = h.status()
+            total_data_transfered = s.total_upload
+            total_payload_transfered = s.total_payload_upload
+            # state_str = ['queued', 'checking', 'downloading metadata', 'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
 
-        time.sleep(1)
+            # print('\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
+            #     (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state]))
+            sys.stdout.flush()
+
+            time.sleep(1)
+        ses.remove_torrent(h)
+        print(f"Seeding {i} of {name} complete")
+        i+=1
+        print("Upload data : ", total_data_transfered, name)
+        print("total payload : ", total_payload_transfered, name)
+    
 
 # Paths to the files you want to share
 file_paths = ['A_10kB', 'A_100kB', 'A_1MB', 'A_10MB']
+# file_paths = ['A_10kB']
 
 # Tracker URL
 tracker_url = "udp://tracker.openbittorrent.com:80/announce"
@@ -55,6 +72,9 @@ for i, file_path in enumerate(file_paths):
     torrent_data = create_torrent(file_path, tracker_url)
     with open(f"{file_path}.torrent", "wb") as f:
         f.write(lt.bencode(torrent_data))
+
+# while True:
+#     seed_torrent("A_10kB.torrent", ".", 6881)
 
 # Seed each torrent in a separate thread
 
